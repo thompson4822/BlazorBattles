ad4b24acf8e62cedb138375b0612346fb87eac1f
steve thompson <thompson4822@protonmail.com>
2020-11-27 12:29:10 -0700

    Additions and/or modifications: - Added army component with styling - Improved Unit Service to know about icons - Added army route to the NavMenu

--- Client/Services/IUnitService.cs
+++ Client/Services/IUnitService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using BlazorBattles.Shared.Entities;
@@ -6,9 +7,32 @@ namespace BlazorBattles.Client.Services
 {
     public interface IUnitService
     {
-       IList<Unit> Units { get; }
-       IList<UserUnit> MyUnits { get; set; }
-       void AddUnit(int unitId);
+        /// <summary>What types of units exist?</summary>
+        IList<Unit> Units { get; }
+
+        /// <summary>
+        /// What icon should we use for the given user unit?
+        /// </summary>
+        /// <param name="userUnit"></param>
+        /// <returns></returns>
+        string IconFor(UserUnit userUnit);
+        
+        /// <summary>What units do I currently have (what is my army)?</summary>
+        IList<UserUnit> MyUnits { get; set; }
+        
+        /// <summary>
+        /// Add a unit to the army using the given unit id
+        /// </summary>
+        /// <param name="unitId">the id of the unit</param>
+        void AddUnit(int unitId);
+        
+        /// <summary>
+        /// What unit matches the given unit id?
+        /// </summary>
+        /// <param name="id">the unit id</param>
+        /// <returns></returns>
+        Unit UnitFor(int id);
+
     }
 
     class UnitService : IUnitService
@@ -20,11 +44,24 @@ namespace BlazorBattles.Client.Services
             new Unit {Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200},
         };
 
+        public string IconFor(UserUnit unit)
+        {
+            Dictionary<int, string> iconMap = new()
+            {
+                {1, "icons/W_Sword006.png"},
+                {2, "icons/S_Bow08.png"},
+                {3, "icons/C_Hat01.png"},
+            };
+            return iconMap[unit.UnitId];
+        }
+
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
+
+        public Unit UnitFor(int id) => Units.FirstOrDefault(unit => unit.Id == id);
         
         public void AddUnit(int unitId)
         {
-            var unit = Units.First(unit => unit.Id == unitId);
+            var unit = UnitFor(unitId);
             MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
         }
     }
