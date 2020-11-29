14029237214b88426cf48f1d293fa7bc9c7fd734
steve thompson <thompson4822@protonmail.com>
2020-11-29 15:59:41 -0700

    Additions and/or modifications: - Added SQLite DB - Added database migrations - Added CRUD to the UnitController

--- Server/Controllers/UnitController.cs
+++ Server/Controllers/UnitController.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using BlazorBattles.Server.Data;
 using BlazorBattles.Shared.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace BlazorBattles.Server.Controllers
@@ -10,24 +13,65 @@ namespace BlazorBattles.Server.Controllers
     [ApiController]
     public class UnitController : ControllerBase
     {
-        public IList<Unit> Units { get; } = new List<Unit>
-        {
-            new Unit {Id = 1, Title = "Knight", Attack = 10, Defense = 10, BananaCost = 100},
-            new Unit {Id = 2, Title = "Archer", Attack = 15, Defense = 5, BananaCost = 150},
-            new Unit {Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200},
-        };
+        private readonly DataContext _dataContext;
 
         private readonly ILogger<UnitController> _logger;
 
-        public UnitController(ILogger<UnitController> logger)
+        public UnitController(ILogger<UnitController> logger, DataContext dataContext)
         {
             _logger = logger;
+            _dataContext = dataContext;
         }
 
+        private async Task<IList<Unit>> FetchUnits() => await _dataContext.Units.ToListAsync();
+        
         [HttpGet]
         public async Task<IActionResult> GetUnits()
         {
-            return await Task.Run(() => Ok(Units));
+            return Ok(await FetchUnits());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            await _dataContext.Units.AddAsync(unit);
+            await _dataContext.SaveChangesAsync();
+            return Ok(await FetchUnits());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            Unit dbUnit = await _dataContext.Units.FirstOrDefaultAsync(u => u.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with the given id doesn't exist");
+            }
+
+            // I think this is where C# 9's records would be really useful
+            dbUnit.Title = unit.Title;
+            dbUnit.Attack = unit.Attack;
+            dbUnit.Defense = unit.Defense;
+            dbUnit.BananaCost = unit.BananaCost;
+            dbUnit.HitPoints = unit.HitPoints;
+
+            await _dataContext.SaveChangesAsync();
+            return Ok(dbUnit);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            Unit dbUnit = await _dataContext.Units.FirstOrDefaultAsync(u => u.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with the given id doesn't exist");
+            }
+
+            _dataContext.Units.Remove(dbUnit);
+            await _dataContext.SaveChangesAsync();
+            return Ok(await FetchUnits());
+        }
+        
     }
 }
---
+++ Server/Migrations/20201129220220_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorBattles.Server.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Units",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Attack = table.Column<int>(type: "INTEGER", nullable: false),
+                    Defense = table.Column<int>(type: "INTEGER", nullable: false),
+                    HitPoints = table.Column<int>(type: "INTEGER", nullable: false),
+                    BananaCost = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Units", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Units");
+        }
+    }
+}
