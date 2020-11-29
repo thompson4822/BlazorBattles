9984fa50d12bc3717c01a394215357f666bd39e3
steve thompson <thompson4822@protonmail.com>
2020-11-29 12:20:24 -0700

    Additions and/or modifications: - Added Logout to NavMenu, which removes the flag from local storage - Updated CustomAuthStateProvider to always notify when performing authentication.

--- Client/CustomAuthStateProvider.cs
+++ Client/CustomAuthStateProvider.cs
@@ -17,27 +17,27 @@ namespace BlazorBattles.Client
         
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            var user = new ClaimsPrincipal();
+            
             // when the Login component calls upon us, we'll wait for the isAuthenticated to be set
             // TODO - Here, we don't actually care if it is set to true or false (?!)
             if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
             {
-                // then we're going to establish the identity, a user with that identity, and an authentication
-                // state for that user
+                // then we're going to establish the identity and a user with that identity
                 var identity = new ClaimsIdentity(
                     new[]
                     {
                         new Claim(ClaimTypes.Name, "Steve")
                     }, "test authentication type"
                 );
-                var user = new ClaimsPrincipal(identity);
-                var state = new AuthenticationState(user);
-                
-                // next we want to fire an event indicating the authentication changed and return the state.
-                NotifyAuthenticationStateChanged(Task.FromResult<AuthenticationState>(state));
-                return state;
+                user = new ClaimsPrincipal(identity);
             }
-            // finally, if "isAuthenticated" wasn't added to local storage, we basically indicate not authenticated.
-            return new AuthenticationState(new ClaimsPrincipal());
+            // Whoever the user (authenticated or not) build a state
+            var state = new AuthenticationState(user);
+                
+            // next we want to fire an event indicating the authentication changed and return the state.
+            NotifyAuthenticationStateChanged(Task.FromResult<AuthenticationState>(state));
+            return state;
         }
     }
 }
