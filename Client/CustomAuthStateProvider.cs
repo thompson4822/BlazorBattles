0fd4899b1441ed1951b09daba12f09364ba594f7
steve thompson <thompson4822@protonmail.com>
2020-11-29 11:44:05 -0700

    Additions and/or modifications: - Added LocalStorage dependency - Changed login to be a component. When submitted, it tells the app that the user is authenticated. - Incorporated the Login component into the Index.razor page - Made top menu just redirect to home in the case of login.

--- Client/CustomAuthStateProvider.cs
+++ Client/CustomAuthStateProvider.cs
@@ -1,23 +1,43 @@
 
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazorBattles.Client
 {
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        private readonly ILocalStorageService _localStorageService;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorageService)
         {
-            //return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
-            var identity = new ClaimsIdentity(
-                new[]
-                {
-                    new Claim(ClaimTypes.Name, "Steve")
-                }, "test authentication type"
-            );
-            var user = new ClaimsPrincipal(identity);
-            return Task.FromResult<AuthenticationState>(new AuthenticationState(user));
+            _localStorageService = localStorageService;
+        }
+        
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            // when the Login component calls upon us, we'll wait for the isAuthenticated to be set
+            // TODO - Here, we don't actually care if it is set to true or false (?!)
+            if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
+            {
+                // then we're going to establish the identity, a user with that identity, and an authentication
+                // state for that user
+                var identity = new ClaimsIdentity(
+                    new[]
+                    {
+                        new Claim(ClaimTypes.Name, "Steve")
+                    }, "test authentication type"
+                );
+                var user = new ClaimsPrincipal(identity);
+                var state = new AuthenticationState(user);
+                
+                // next we want to fire an event indicating the authentication changed and return the state.
+                NotifyAuthenticationStateChanged(Task.FromResult<AuthenticationState>(state));
+                return state;
+            }
+            // finally, if "isAuthenticated" wasn't added to local storage, we basically indicate not authenticated.
+            return new AuthenticationState(new ClaimsPrincipal());
         }
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using BlazorBattles.Client.Services;
+using Blazored.LocalStorage;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -21,6 +22,7 @@ namespace BlazorBattles.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddBlazoredToast();
+            builder.Services.AddBlazoredLocalStorage();
             
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
